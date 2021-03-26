7d5f05b917e5a24f17e101362b2014aa93394855
Chyngyz21 <80499988+Chyngyz21@users.noreply.github.com>
2021-03-26 10:11:43 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
+<img src="images.jpeg">
 <h1>Profile</h1> 
 <tr>Name:</tr>
 <td>Chyngyz</br>Surname: Rustam uulu</td>
 </br><td>Age: 18</td>
 </br><td>Studying: Alatoo University<td/>
 </br><td>Course: 3rd<td/>
-<img src="images.jpeg">
